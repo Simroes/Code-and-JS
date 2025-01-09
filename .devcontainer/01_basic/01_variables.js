e119c86c2523e8adcc230657d11d726e8be8aa33
Simroes <mrinalika4474@gmail.com>
2025-01-09 08:33:28 +0000

    variable-notes

--- .devcontainer/01_basic/01_variables.js
+++ .devcontainer/01_basic/01_variables.js
@@ -3,6 +3,8 @@ var accountname =" mrinalika"
 let accountEmail = "email.tee@gmail.com"
 accountCity =" Hyderabad"
 
-
+/*
+prefer not to use var because of issue in block scope & functional scope
+*/
 console.log(accountEmail);
 console.table([accountid, accountname, accountEmail, accountCity])
