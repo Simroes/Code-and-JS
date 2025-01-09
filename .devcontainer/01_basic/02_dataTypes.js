2998f1ce57eaa127517a4bfea9cbfcdbfd3d7600
Simroes <mrinalika4474@gmail.com>
2025-01-09 08:50:13 +0000

    data_types

---
+++ .devcontainer/01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(typeof null) //=> object
+console.log(typeof undefined) //=> undefined
+//alert("hello"+ 3+3) - we are using nodejs not browser
+//code readability shouldbe high
+//follow mdn documentation - easy to understand but it's not original documentation
+//original documentation is present in tc39.e i.e. ecma script
+
+//number range = 2^53
+//bigint for larger numbers
+//string => prefer to use in double quotes
+//boolean => true or false
+//null => standalone value & it's also a datatype , representation of empty value
+//undefined => when value is not defined
+// symbok => unique
+
+
+// Object
