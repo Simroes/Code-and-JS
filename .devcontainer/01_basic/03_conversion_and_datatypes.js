82ebcda16b25cc03283c8ea547998bf5c7d7d397
Simroes <mrinalika4474@gmail.com>
2025-01-09 10:54:20 +0000

    conversio of dt

---
+++ .devcontainer/01_basic/03_conversion_and_datatypes.js
@@ -0,0 +1,28 @@
+let score = "33"
+console.log(typeof(score));
+
+let valueInNum = Number(score)
+console.log(typeof(valueInNum));
+
+//"33" => 33
+//"33bc" => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+console.log(typeof booleanisLoggedIn)
+
+//1 => true
+//0 => false
+//"" =>false
+//"any string" => true
+
+
+let sumnum=33
+let strnum = String(sumnum)
+console.log(strnum)
+console.log(typeof strnum)
+
+// 33 => "33"
