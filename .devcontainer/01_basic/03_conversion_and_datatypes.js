5327b30f7c5f709c6b86ae1ae9d00aa50fd54777
Simroes <mrinalika4474@gmail.com>
2025-01-09 11:08:13 +0000

    operation

--- .devcontainer/01_basic/03_conversion_and_datatypes.js
+++ .devcontainer/01_basic/03_conversion_and_datatypes.js
@@ -1,8 +1,8 @@
 let score = "33"
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueInNum = Number(score)
-console.log(typeof(valueInNum));
+//console.log(typeof(valueInNum));
 
 //"33" => 33
 //"33bc" => NaN
@@ -11,8 +11,8 @@ console.log(typeof(valueInNum));
 
 let isLoggedIn = 1
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
-console.log(typeof booleanisLoggedIn)
+//console.log(booleanisLoggedIn)
+//console.log(typeof booleanisLoggedIn)
 
 //1 => true
 //0 => false
@@ -22,7 +22,21 @@ console.log(typeof booleanisLoggedIn)
 
 let sumnum=33
 let strnum = String(sumnum)
-console.log(strnum)
-console.log(typeof strnum)
+//console.log(strnum)
+//console.log(typeof strnum)
 
 // 33 => "33"
+
+
+
+//**************OPERATIONS***************/
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+console.log("2"+2+2) //"222"
+console.log(2+2+"2") //"42"
+console.log(+true) //1
+
+
