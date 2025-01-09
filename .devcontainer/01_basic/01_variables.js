ef98e7bfc040c912c57189387d54a37ec6822d12
Simroes <mrinalika4474@gmail.com>
2025-01-09 08:25:49 +0000

    added variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountid="12345"
+var accountname =" mrinalika"
+let accountEmail = "email.tee@gmail.com"
+accountCity =" Hyderabad"
+
+
+console.log(accountEmail);
+console.table([accountid, accountname, accountEmail, accountCity])
